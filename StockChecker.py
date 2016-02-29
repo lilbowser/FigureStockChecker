1c0d36cb314b48108aa775c5df6e7a6c4474f934
Josh <ham@x303.com>
2016-02-29 13:06:19 -0500

    --StockChecker-- Implemented Searching for figures using a combination of Regex and Fuzzy Wuzzy token set ration

--- StockChecker.py
+++ StockChecker.py
@@ -1,15 +1,21 @@
-import requests # pip3 install requests
+import requests  # pip3 install requests
 from bs4 import BeautifulSoup  # pip3 install beautifulsoup4
 import xml.etree.ElementTree as ET
 from urllib.parse import urljoin
 import time
 from chump import Application  # pip3 install chump
+import re
+
+from fuzzywuzzy import fuzz #  pip3 install fuzzywuzzy  #  http://chairnerd.seatgeek.com/fuzzywuzzy-fuzzy-string-matching-in-python/
+from fuzzywuzzy import process
+
+from verbalexpressions import VerEx
 
 class WebsiteData:
 
     def __init__(self, website_xml):
         """
-        This is a data type for holding and processing all info relating to a webpage
+        This is a data type for holding and processing all info relating to a base webpage (AKA The root of a collection of search pages)
         @param website_xml: An ElementTree holding all xml information regarding a base website.
         @type website_xml: ElementTree
         @return: None
@@ -25,14 +31,22 @@ class WebsiteData:
             for sub_site_xml in self._website_xml.findall('sub_site'):
                 self._sub_sites.append(SubSiteData(sub_site_xml))
 
-            for subSite in self._sub_sites:
-                print(self._base_url + subSite.url)
-
+            # for subSite in self._sub_sites:
+                # print(self._base_url + subSite.url)
         except Exception as e:
             print("Could not load data for " + self._website_name + ". Error: " + str(e))
             self._website_xml = None
             self._base_url = ""
             self._sub_sites = None
+
+    @property
+    def website_name(self):
+        """
+        Returns the english name of the website held by this object
+        @return: Website Name
+        @rtype: str
+        """
+        return self._website_name
 
     @property
     def sub_sites(self):
@@ -42,6 +56,7 @@ class WebsiteData:
         @rtype: list[SubSiteData]
         """
         return self._sub_sites
+
     @property
     def url(self):
         """
@@ -52,34 +67,44 @@ class WebsiteData:
         return self._base_url
 
 
-
-
 class SubSiteData:
 
     def __init__(self, sub_site_xml):
+        """
+        This is a data type for holding and processing all info relating to a sub-site specified for a base webpage
+        @param sub_site_xml: An ElementTree holding xml information regarding a sub website
+        @type sub_site_xml: ElementTree
+        @return: None
+        @rtype: None
+        """
         self._xml = sub_site_xml
         self._url = self._xml.find('url').text
+        self.website_html = None
+        self._figures = None
+        self.figure_search_data = []
 
-        self._website_data = None
-        self._website_soup = None
-        self.product_soup = None
-        self.figure_soup = None
-
-        self._figures = []
-
+        # initialize the search parameters.
         for fig in self._xml.findall('figure'):
-            self._figures.append(FigureSearchData(fig))
-
-        self._scraped_figures = []
+            self.figure_search_data.append(SearchParams(fig))
 
     @property
     def figures(self):
         """
         A collection of figure objects
-        @return: list[Figure]
-        @rtype: list[FigureSearchData]
+        @return: list[FigureData]
+        @rtype: list[FigureData]
         """
         return self._figures
+
+    @figures.setter
+    def figures(self, value):
+        """
+        A collection of figures scraped from a website
+
+        @param value:
+        @type value:
+        """
+        self._figures = value
 
     @property
     def url(self):
@@ -89,136 +114,272 @@ class SubSiteData:
         @rtype: str
         """
         return self._url
-    @property
-    def website_data(self):
-        """
-        Returns all HTML data scraped from the website
-        @return: Website associated with subsite url
-        @rtype: str | None
-        """
-        return self._website_data
-    @website_data.setter
-    def website_data(self, value):
-        """
-        Stores the website HTML data and computes the soup from it
-        @param value: Website HTML
-        @type value: str
-        @return:
-        @rtype:
-        """
-        self._website_data = value
-        if self._website_data is not None:
-            self._website_soup = BeautifulSoup(self._website_data, "html.parser")
-        else:
-            self._website_soup = None
-
-    @property
-    def website_soup(self):
-        """
-        Returns all HTML soup from the website
-        @return: Website Soup associated with subsite url
-        @rtype: BeautifulSoup | None
-        """
-        return self._website_soup
-
-    # @property
-    # def new_figure(self):
-    #     """
-    #     creates a
-    #     @return: Website associated with subsite url
-    #     @rtype: str | None
-    #     """
-    #     return self._website_data
-    # @new_figure.setter
-    # def new_figure(self, value):
-    #     """
-    #     Stores the website HTML data and computes the soup from it
-    #     @param value: Website HTML
-    #     @type value: str
-    #     @return:
-    #     @rtype:
-    #     """
-    #     self._website_data = value
-
-    def new_figure(self, name=None, link=None, price=None, picture_link=None):
-        """
-        Creates a new scraped figure from soup and adds it to the database.
-
-        @return:
-        @rtype:
-        """
-        self._scraped_figures.append(FigureData(name, link, price, picture_link))
 
 
+class FigureSearchData:
 
-class FigureData:
-    def __init__(self, figure_name=None, link=None, price=None, picture_link=None):
-        self._figure_name = figure_name
-        self._link = link
-        self._price = price
-        self._picture_link = picture_link
-        self._condition = None  # type: str
+    def __init__(self, search_param_xml):
+        self._xml = search_param_xml
+        self.regEx_string = '(.*)'
 
-        @property
-        def condition(self):
-            return self._condition
+        self.search_parameter = self._xml.text
+        try:
+            self.dependence = self._xml.attrib['dependence']
+        except KeyError as e:
+            print("ln 129: " + str(e))
 
-        @condition.setter
-        def condition(self, value):
-            if self._condition is None:
-                tmp = value[value.rindex('/') + 1:]
-                # TODO: We need to move the selection of the decoder out of this class
-                self._condition = JungleDecoder.conditionDecode[tmp]
-            else:
-                raise ValueError("Can not set condition once condition has already been set.")
+        self.search_parameter_escaped = re.escape(self.search_parameter)
+        self.tester = ver_ex.anything().find(self.search_parameter).anything().with_any_case(True)
+
+        try:
+            self.exactly = bool(self._xml.attrib['exactly'])
+        except KeyError as e:
+            self.exactly = False
+
+        print(self.exactly)
 
 
-class FigureSearchData(FigureData):
+class SearchParams:
 
     def __init__(self, figure_xml):
-        FigureData.__init__()
         self._xml = figure_xml
 
         self._figure_name = self._xml.attrib['name']
         self._search_parameters = []
 
         for search_xml in self._xml.findall('search'):
-            self._search_parameters.append(SearchParam(search_xml))
+            self._search_parameters.append(FigureSearchData(search_xml))
+
+        self.fuzzy_search = ""
+        self.regex_search = None
 
     @property
-    def figure_name(self):
+    def parameters(self):
+        return self._search_parameters
+
+    @parameters.setter
+    def parameters(self, value):
+        self._search_parameters = value
+
+    def search(self, _figure, confidence):
+        # we are using a two step matching system.
+        # First we be above a confidence threashold using a fuzzy search
+        # Then we must find all of the mandatory parameters using regex
+
+        for param in self._search_parameters:
+            self.fuzzy_search += param.search_parameter + " "
+
+            if param.dependence == "mandatory":
+                if param.exactly:
+                    param.regEx_string = r'\b' + re.escape(param.search_parameter) + r'\b'
+                else:
+                    param.regEx_string = param.search_parameter
+
+            # if self.regex_search is None:
+            #     self.regex_search = param.regEx_string
+            # else:
+            #     self.regex_search += "|" + param.regEx_string
+
+        if fuzz.token_set_ratio(self.fuzzy_search, figure.name) > confidence:
+            # Initial match
+            for param in self._search_parameters:
+                if re.search(param.regEx_string, figure.name) is None:
+                    # if any of the mandatory strings are not found, return false
+                    return False
+        else:
+            return False
+
+        return True
+
+
+class Figures:
+    # TODO: Figure out a better way of doing this.
+    def __init__(self, service, html, iurl):
+
+        self._url = iurl
+        self._service = service.lower()  # type: str
+        self._html = html
+        self._decoder = Decoder(self._service)
+        self.figures = self._decoder.get_figures(self._html, self._url)  # type: list[FigureData]
+
+
+class FigureData:
+
+    def __init__(self, service, figure_html):
+        self._service = service.lower()  # type: str
+
+        self._html = figure_html
+        # self._parsed_html = BeautifulSoup(localHTML, 'html.parser')
+        self._name = None  # type: str
+        self.price = None  # type: str
+        self.link = None  # type: str
+        self.pic_link = None  # type: str
+        self._condition = None  # type: str
+        self._releaseStatus = None  # type: str
+
+    @property
+    def release_status(self):
+        return self._releaseStatus
+
+    @release_status.setter
+    def release_status(self, value):
+        self._releaseStatus = value
+
+    @property
+    def name(self):
         """
-        The name of the figure we are searching for
-        @return: figure name
+        Returns the name of the figure
+
+        @return: Figure Name
         @rtype: str
         """
-        return self._figure_name
+        return self._name
 
+    @name.setter
+    def name(self, value):
+        self._name = value
 
-class SearchParam:
+    @property
+    def condition(self):
+        """
+        Returns the condition of the figure.
 
-    def __init__(self, search_param_xml):
-        self._xml = search_param_xml
-        self.search_parameter = self._xml.text
-        self.dependence = self._xml.attrib['dependence']
+        @return: a condition string defined in Decoder
+        @rtype: str
+        """
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        if self._condition is None:
+            self._condition = value
+        else:
+            raise ValueError("Can not change condition once condition has already been set.")
 
 
 class Decoder:
+    jungle = 'jungle'
+    amiami = 'amiami'
 
-    def condition(self, value):
+    def __new__(cls, service, *arguments, **keyword):
+        for subclass in Decoder.__subclasses__():
+            if service.lower().startswith(subclass.service):
+                return super(cls, subclass).__new__(subclass)#, *arguments)#, **keyword)
+        raise Exception('Website not supported not supported')
+
+    def _condition(self, value):
+        raise NotImplementedError
+
+    def _get_pages(self):
+        raise NotImplementedError
+
+    def get_figures(self, html=None, url=None):
         raise NotImplementedError
 
 
 class JungleDecoder(Decoder):
-
+    service = Decoder.jungle
     conditionBase = "http://jungle-scs.co.jp/sale_en/wp-content/themes/jungle_2013en/images/"
     conditionS = "conditionicon_s_en.gif"
     conditionA = "conditionicon_a_en.gif"
     conditionB = "conditionicon_b_en.gif"
     conditionDecode = {conditionS: 'Sealed', conditionA: 'A', conditionB: 'B'}
 
-    def condition(self, value):
-        return self.conditionDecode[value]
+    def __new__(cls, service):
+        pass
+
+    def __init__(self, service):
+        self._product_phtml = None
+        self._parsed_html = None  # type: BeautifulSoup
+        self._figures = []  # type: list[FigureData]
+
+
+        # if self.product_phtml is None:
+
+    def _condition(self, value):
+        tmp = value[value.rindex('/') + 1:]
+        return self.conditionDecode[tmp]
+
+    def _get_pages(self):
+        paging_tags = self._parsed_html.find(id='paging')  #.find_all('span')  # type: list[tag]
+
+        if paging_tags is not None:
+            next_page_element = paging_tags.find('span', string='Next Page»') #_class='sp04_pl20')  #.find('a').get('href')
+            if next_page_element is not None:
+                next_page_url = next_page_element.find('a').get('href')
+                print("getting " + next_page_url)
+                return next_page_url
+
+        return None
+
+    def get_figures(self, html=None, url=None):
+
+        if html is not None and len(self._figures) < 1 and url is not None:
+
+            # Only parse if html is given and the figures array is empty
+            more_figures = True
+            next_page_url = None
+            while more_figures:
+                self._parsed_html = BeautifulSoup(html, 'html.parser')
+                try:
+                    next_page_url = self._get_pages()
+
+                    products_soup = self._parsed_html.find(id='products').find_all("li")
+
+                    for figure_soup in products_soup:
+                        tempFig = FigureData(Decoder.jungle, html)
+
+                        tempFig.name = figure_soup.find(class_='wrapword').text
+
+                        tempFig.price = figure_soup.find(class_="price").text
+
+                        tempFig.pic_link = figure_soup.find('img')['src']
+
+                        tempFig.condition = self._condition(figure_soup.find('p').find_all('img')[1]['src'])
+
+                        relURL = figure_soup.find('a').get('href')
+                        tempFig.link = urljoin(url, relURL)
+
+                        self._figures.append(tempFig)
+                    #
+                except Exception as e:
+                    print("Try Failed")
+                    print(e)
+
+                if next_page_url is not None:
+                    html = requests.get(next_page_url).text
+                    print("got next page")
+                else:
+                    more_figures = False
+
+        return self._figures
+
+
+class AmiAmiDecoder(Decoder):
+    service = Decoder.amiami
+    conditionBase = "http://amiami.co.jp"
+    conditionS = "conditionicon_s_en.gif"
+    conditionA = "conditionicon_a_en.gif"
+    conditionB = "conditionicon_b_en.gif"
+    conditionDecode = {conditionS: 'Sealed', conditionA: 'A', conditionB: 'B'}
+
+    def __new__(cls, service):
+        pass
+
+    def __init__(self, service):
+        self._product_phtml = None
+        self._parsed_html = None
+        self._figures = [] # type: list[FigureData]
+
+    def _condition(self, value):
+        raise NotImplementedError
+        # return self.conditionDecode[value]
+
+    def _get_pages(self):
+        raise NotImplementedError
+
+    def get_figures(self, html=None):
+        raise NotImplementedError
 
 
 def scrapeSite(_url):
@@ -248,11 +409,11 @@ def scrapeSite(_url):
     return website_data
 
 
-
-
 if __name__ == '__main__':
     push_app = Application("***REMOVED***")
     push_User = push_app.get_user("***REMOVED***")
+
+    ver_ex = VerEx()
 
     tree = ET.parse('sources.xml')
     xmlData = tree.getroot()
@@ -267,22 +428,23 @@ if __name__ == '__main__':
         if site.sub_sites is not None:
 
             for sub_site in site.sub_sites:
-                sub_site.website_data = scrapeSite(site.url + sub_site.url)
-                products_soup = []
+                url = site.url + sub_site.url
+                sub_site.website_html = scrapeSite(site.url + sub_site.url)
+                sub_site.figures = Figures(site.website_name, sub_site.website_html, url).figures
 
-                if sub_site.website_soup is not None:
-                    sub_site.product_soup = sub_site.website_soup.find(id="products")
-                    sub_site.figure_soup = sub_site.product_soup.find_all('li')
-
-                    for figure in sub_site.figure_soup:
-
-                        relURL = figure.find('a').get('href')
-                        figure_link = urljoin(site.url + sub_site.url, relURL)
-                        sub_site.new_figure(name=figure.find(class_="wrapword").text,
-                                            price=figure.find(class_="price").text,
-                                            picture_link=figure.find('img')['src'],
-                                            link=figure_link)
-                        print("woo")
+    for site in websites:
+        if site.sub_sites is not None:
+            for sub_site in site.sub_sites:
+                for figure in sub_site.figures:
+                    for search_data in sub_site.figure_search_data:
+                        fig_found = search_data.search(figure, 60)
+                        # if search_data.tester.match(figure.name):
+                        #     print("we matched " + figure.name)
+                        #     break
+                        # result = fuzz.token_set_ratio(searchString, figure.name)
+                        # if result > 40:
+                        if fig_found:
+                            print("Matched figure " + figure.name) # + " with " + str(result) + "% confidence against " + searchString )
 
 
     #Search for Items
